9f7c1fe30aa7ee80d75e3b4da92f25f7f4ea76e9
Rempty <rempty.15@gmail.com>
2015-05-09 19:07:12 -0500

    IMG size meta box
    Arreglo del tamaño de las imágenes
    Mejor diseño del repeater

--- meta-box-class/my-meta-box-class.php
+++ meta-box-class/my-meta-box-class.php
@@ -386,7 +386,7 @@ class AT_Meta_Box {
            $mmm =  isset($me[$field['fields'][0]['id']])? $me[$field['fields'][0]['id']]: "";
            if ( in_array( $field['fields'][0]['type'], array('image','file') ) )
             $mmm = $c +1 ;
-           echo '<div class="at-repater-block">'.$mmm.'<br/><table class="repeater-table" style="display: none;">';
+           echo '<div class="at-repater-block"><h5 class="repeater_title">'.$mmm.'</h5><table class="repeater-table" style="display: none;">';
            if ($field['inline']){
              echo '<tr class="at-inline" VALIGN="top">';
            }
